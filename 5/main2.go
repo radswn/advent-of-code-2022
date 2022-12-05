3dadecbff40ae90d10c9e9b0245b30064e3a5cf9
Radosław Winiecki <r.winiecki@outlook.com>
2022-12-05 09:30:04 +0100

    day 5

---
+++ 5/main2.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, _ := os.Open("5/input.txt")
+	defer file.Close()
+
+	fileScanner := bufio.NewScanner(file)
+	fileScanner.Split(bufio.ScanLines)
+
+	cargos := map[int]string{
+		1: "FHBVRQDP",
+		2: "LDZQWV",
+		3: "HLZQGRPC",
+		4: "RDHFJVB",
+		5: "ZWLC",
+		6: "JRPNTGVM",
+		7: "JRLVMBS",
+		8: "DPJ",
+		9: "DCNWV",
+	}
+
+	for i := 0; i < 10; i++ {
+		fileScanner.Scan()
+	}
+
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		line = strings.Replace(line, "move ", "", 1)
+		line = strings.Replace(line, "from ", "", 1)
+		line = strings.Replace(line, "to ", "", 1)
+
+		e := strings.Split(line, " ")
+
+		move, _ := strconv.Atoi(e[0])
+		from, _ := strconv.Atoi(e[1])
+		to, _ := strconv.Atoi(e[2])
+
+		splitIndex := len(cargos[from]) - move
+
+		toCut := cargos[from][splitIndex:]
+		cargos[from] = cargos[from][:splitIndex]
+		cargos[to] = cargos[to] + toCut
+	}
+
+	fmt.Println(cargos)
+}
--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 - [Day 2: Rock Paper Scissors](https://adventofcode.com/2022/day/2)
 - [Day 3: Rucksack Reorganization](https://adventofcode.com/2022/day/3)
 - [Day 4: Camp Cleanup](https://adventofcode.com/2022/day/4)
-- Day 5:
+- [Day 5: Supply Stacks](https://adventofcode.com/2022/day/5)
 - Day 6:
 - Day 7:
 - Day 8:
