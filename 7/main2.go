b27095784b3f462c7a53073681821b9a60ddad50
Radosław Winiecki <r.winiecki@outlook.com>
2022-12-07 09:38:45 +0100

    day 7

---
+++ 7/main2.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, _ := os.Open("7/input.txt")
+	defer file.Close()
+
+	fileScanner := bufio.NewScanner(file)
+	fileScanner.Split(bufio.ScanLines)
+
+	dirStack := make([]string, 0)
+	dirSize := map[string]int{}
+
+	for fileScanner.Scan() {
+		text := fileScanner.Text()
+
+		if string(text[0]) == "$" {
+			if string(text[2:4]) == "cd" {
+				dest := strings.Split(text, " ")[2]
+
+				if dest == ".." {
+					dirStack = dirStack[:len(dirStack)-1]
+				} else {
+					fullPath := strings.Join(dirStack, "/") + dest
+					dirSize[fullPath] = 0
+					dirStack = append(dirStack, dest)
+				}
+			}
+		} else {
+			if string(text[0]) != "d" {
+				for i, _ := range dirStack {
+					dirStackSlice := dirStack[:i + 1]
+					fullPath := strings.Join(dirStackSlice, "/")
+					fileSize, _ := strconv.Atoi(strings.Split(text, " ")[0])
+					dirSize[fullPath] += fileSize
+				}
+			}
+		}
+	}
+
+	unusedSpace := 70000000 - dirSize["/"]
+	requiredSpace := 30000000 - unusedSpace
+
+	dirToDeleteSize := dirSize["/"]
+
+	for _, v := range dirSize {
+		if v >= requiredSpace && v < dirToDeleteSize{
+			dirToDeleteSize = v
+		}
+	}
+	fmt.Println(dirToDeleteSize)
+}
--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 - [Day 4: Camp Cleanup](https://adventofcode.com/2022/day/4)
 - [Day 5: Supply Stacks](https://adventofcode.com/2022/day/5)
 - [Day 6: Tuning Trouble](https://adventofcode.com/2022/day/6)
-- Day 7:
+- [Day 7: No Space Left On Device](https://adventofcode.com/2022/day/7)
 - Day 8:
 - Day 9:
 - Day 10:
